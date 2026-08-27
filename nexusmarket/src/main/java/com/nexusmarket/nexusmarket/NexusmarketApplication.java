1214ecb488dd858b08e5c241cf9fdef086042db3
jpmontoyag <jpmontoyag@outlook.es>
2026-08-27 18:18:40 -0500

    Create initial code

---
+++ nexusmarket/src/main/java/com/nexusmarket/nexusmarket/NexusmarketApplication.java
@@ -0,0 +1,13 @@
+package com.nexusmarket.nexusmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusmarketApplication.class, args);
+	}
+
+}
---
+++ nexusmarket/src/test/java/com/nexusmarket/nexusmarket/NexusmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusmarket.nexusmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
