b110725a4f36e89743272d4c27cfdb36e95b1a1e
jpmontoyag <jpmontoyag@outlook.es>
2026-08-27 18:24:11 -0500

    Implement class Person

---
+++ nexusmarket/src/main/java/com/nexusmarket/nexusmarket/domain/models/Person.java
@@ -0,0 +1,8 @@
+public abstract class Person {
+    private String identification;
+    private String full_name;
+    private String email;
+    private String role;
+    private String state;
+
+}
